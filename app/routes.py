a9eacc7e38ecae7f427138cfe0d60569ba1731b0
Ali Alohali <alioh@about.me>
2018-08-08 21:57:21 +0300

    Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
+from flask import render_template
 from app import micro
+
 
 @micro.route('/')
 @micro.route('/index')
 
 def index():
-    return('Hello, World!')
+    
+    user = {'username': 'Ali'}
+    title = 'Welcome to Flask'
+
+    posts = [
+        {
+            'author': {'username': 'NotAli'},
+            'post': 'What is Flask?'
+        },
+        {
+            'author': {'username': 'MaybeAli'},
+            'post': "I don't know what I am doing!"
+        }
+    ]
+
+
+    return render_template('index.html', title=title, user=user, posts=posts)
