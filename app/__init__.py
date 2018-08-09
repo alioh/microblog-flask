ea68a260a94465f0102f7480da830aabaac19ab0
Ali Alohali <alioh@about.me>
2018-08-09 21:47:05 +0300

    login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 micro = Flask(__name__)
+micro.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,15 @@
 from flask import render_template
 from app import micro
+from app.forms import LoginForm
 
 
 @micro.route('/')
 @micro.route('/index')
 
+
 def index():
-    
     user = {'username': 'Ali'}
     title = 'Welcome to Flask'
-
     posts = [
         {
             'author': {'username': 'NotAli'},
@@ -20,6 +20,10 @@ def index():
             'post': "I don't know what I am doing!"
         }
     ]
-
-
     return render_template('index.html', title=title, user=user, posts=posts)
+
+@micro.route('/login')
+def login():
+    form = LoginForm()
+    title = 'Login'
+    return render_template('login.html', title=title, form=form)
