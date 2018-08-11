7accae9966d2f0b602a9e1c9ef0cb6feff3d3ce0
Ali Alohali <alioh@about.me>
2018-08-11 13:56:59 +0300

    login/logot

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from config import Config
-from flask_bootstrap import Bootstrap
+#from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 micro = Flask(__name__)
 micro.config.from_object(Config)
-Bootstrap(micro)
+#Bootstrap(micro)
 db = SQLAlchemy(micro)
 migrate = Migrate(micro, db)
-
+login = LoginManager(micro)
+login.login_view = 'login'
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from app import micro
 from app.forms import LoginForm
+from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
 
 
 @micro.route('/')
 @micro.route('/index')
-
+@login_required
 def index():
-    user = {'username': 'Ali'}
     title = 'Welcome to Flask'
     posts = [
         {
@@ -19,14 +22,28 @@ def index():
             'post': "I don't know what I am doing!"
         }
     ]
-    return render_template('/index.html', title=title, user=user, posts=posts)
+    return render_template('/index.html', title=title, posts=posts)
 
 @micro.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     title = 'Login'
     if form.validate_on_submit():
-        flash('Login request for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        #flash('Login request for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(next_page)
     return render_template('login.html', title=title, form=form)
+
+@micro.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
